1749d98b9867fcdd247c5c8568cc4e2e574b9dfe
Roneyfsilva79 <roneyfsilva79@gmail.com>
2021-08-03 21:15:52 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo.jogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalogo.jogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
---
+++ Catalogo.jogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalogo.jogos.Exceptions
+{
+    public class JogoNaoCadastradoException: Exception
+    {
+        public JogoNaoCadastradoException()
+            :base("Este jogo não está cadastrado")
+        {}
+    }
+}
